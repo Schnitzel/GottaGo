61e01d8eb32d4808c7fb3a72213fee1514b8cb27
Michael Schmid <michael@amazeelabs.com>
2012-04-29 17:51:33 +0200

    packed everything in drupalBehavior

--- sites/all/themes/gottago/js/gottagoForm.js
+++ sites/all/themes/gottago/js/gottagoForm.js
@@ -10,13 +10,19 @@
 // adding a small wrapper around your JavaScript code. See:
 // - http://drupal.org/node/224333#javascript_compatibility
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
-(function ($) {
-  jQuery(function() {
-    jQuery('#edit-field-station-und-0-value').width(jQuery('#gottago-node-form').width() - jQuery('.field-station-prefix').width() - 100);
-    jQuery('.field-widget-options-buttons .option').live('click', function(){
-    	jQuery(this).siblings('input').attr('checked', true);
-      jQuery('.field-widget-options-buttons .option').removeClass('selected');
-      jQuery(this).addClass('selected');
-    });
-  });
+(function ($){
+
+  Drupal.behaviors.initGottagoClickLine = {
+    attach: function(context) {
+
+	    jQuery('.form-item-field-line-und label.option', context).click(function(){
+	    	jQuery(this).siblings('input').attr('checked', true);
+	      jQuery('.form-item-field-line-und label.option').removeClass('selected');
+	      jQuery(this).addClass('selected');
+	    });
+
+    }
+  }
+
+
 })(jQuery);
